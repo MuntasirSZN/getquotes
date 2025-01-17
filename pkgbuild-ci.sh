75efc4ebe434fb17638560a2e6aa7e22567cf563
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-01-17 22:34:22 +0600

    fix(ci): script for pkg

--- pkgbuild-ci.sh
+++ pkgbuild-ci.sh
@@ -8,23 +8,25 @@ TMPDIR=$(mktemp -d)
 trap 'rm -rf "$TMPDIR"' EXIT
 
 # Fetch the latest Git tag and update pkgver
-LATEST_TAG=$(git ls-remote --tags --sort=-v:refname https://github.com/MuntasirSZN/getquotes.git | grep -oP 'tags/\K.*' | head -n 1 | sed 's/^v//')
-if [[ -n "$LATEST_TAG" ]]; then
-	# Escape special characters for sed
-	LATEST_TAG_ESC=$(echo "$LATEST_TAG" | sed 's/[\/&]/\\&/g')
-	# Update the PKGBUILD
-	sed -i "s/^pkgver=.*/pkgver=$LATEST_TAG_ESC/" PKGBUILD
-	echo "Updated pkgver to $LATEST_TAG in PKGBUILD."
-else
+LATEST_TAG=$(git ls-remote --tags https://github.com/MuntasirSZN/getquotes.git | grep -oP 'tags/v?\K([0-9]+(\.[0-9]+)+)' | sort -V | tail -n 1)
+
+if [[ -z "$LATEST_TAG" ]]; then
 	echo "No Git tags found. pkgver was not updated."
 	exit 1
 fi
+
+# Escape special characters for sed
+LATEST_TAG_ESC=$(echo "$LATEST_TAG" | sed 's/[\/&]/\\&/g')
+
+# Update the PKGBUILD
+sed -i "s/^pkgver=.*/pkgver=$LATEST_TAG_ESC/" PKGBUILD
+echo "Updated pkgver to $LATEST_TAG in PKGBUILD."
 
 # Read the updated pkgver from PKGBUILD
 PKGVER=$(sed -n 's/^pkgver=//p' PKGBUILD)
 
 # Read source array from PKGBUILD
-mapfile -t SOURCES < <(sed -n '/^source=(/,/^)/ { /^source=(/d; /^)/d; s/^[[:space:]]*"\(.*\)".*$/\1/; p }' PKGBUILD)
+mapfile -t SOURCES < <(sed -n '/^source=(/,/^)/ { /^source=(/d; /^)/d; /^[[:space:]]*#/d; s/^[[:space:]]*"\([^"]*\)".*$/\1/; p }' PKGBUILD)
 
 # Download files and calculate sha256sums
 declare -A SUMS
@@ -35,17 +37,17 @@ for URL in "${SOURCES[@]}"; do
 	SUMS["$URL"]=$SUM
 done
 
-# Read existing sha256sums from PKGBUILD
-mapfile -t OLD_SUMS < <(sed -n '/^sha256sums=(/,/^)/ { /^sha256sums=(/d; /^)/d; s/^[[:space:]]*"\(.*\)".*$/\1/; p }' PKGBUILD)
+# Read existing sha256sums from PKGBUILD with quotes
+mapfile -t OLD_SUMS_QUOTED < <(sed -n '/^sha256sums=(/,/^)/ { /^sha256sums=(/d; /^)/d; /^[[:space:]]*#/d; s/^[[:space:]]*"\([^"]*\)".*$/"\1"/; p }' PKGBUILD)
 
-# Replace old sums with new sums
+# Replace old sums with new sums, preserving quotes
 for i in "${!SOURCES[@]}"; do
 	URL="${SOURCES[$i]}"
 	NEW_SUM="${SUMS[$URL]}"
-	OLD_SUM="${OLD_SUMS[$i]}"
+	OLD_SUM_QUOTED="${OLD_SUMS_QUOTED[$i]}"
 	# Escape special characters for sed
-	OLD_SUM_ESC=$(echo "$OLD_SUM" | sed 's/[\/&]/\\&/g')
-	NEW_SUM_ESC=$(echo "$NEW_SUM" | sed 's/[\/&]/\\&/g')
+	OLD_SUM_ESC=$(echo "$OLD_SUM_QUOTED" | sed 's/[\/&]/\\&/g')
+	NEW_SUM_ESC=$(echo "\"$NEW_SUM\"" | sed 's/[\/&]/\\&/g')
 	# Update the PKGBUILD
 	sed -i "s/$OLD_SUM_ESC/$NEW_SUM_ESC/" PKGBUILD
 done
