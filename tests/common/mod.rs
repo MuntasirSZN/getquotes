1489cd1f1bf5626a1ddd26e60d83176fe1ee24ca
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-04-12 23:12:54 +0600

    chore: switch to 2024

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,6 +3,6 @@ use tempdir::TempDir;
 
 pub fn setup_temp_home() -> Result<TempDir, Box<dyn Error + Send + Sync>> {
     let temp_dir = TempDir::new("getquotes_test")?;
-    std::env::set_var("HOME", temp_dir.path().to_str().unwrap());
+    unsafe { std::env::set_var("HOME", temp_dir.path().to_str().unwrap()) };
     Ok(temp_dir)
 }
