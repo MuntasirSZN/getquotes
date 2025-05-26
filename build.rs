d0071236329fa01811c151eb3c696add7427cd7a
Muntasir Mahmud <muntasir.joypurhat@gmail.com>
2025-05-26 18:52:16 +0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -7,12 +7,15 @@ include!("src/cli.rs");
 fn main() -> std::io::Result<()> {
     let mut cmd = Args::command();
     let completion_out_dir = Path::new("completions");
-    fs::create_dir_all(completion_out_dir)?;
+
+fs::create_dir_all(completion_out_dir)?;
 
     for shell in Shell::value_variants() {
         generate_to(*shell, &mut cmd, "getquotes", completion_out_dir)?;
     }
     let man_out_dir = Path::new("man");
+
+fs::create_dir_all(man_out_dir)?;
 
     let man = clap_mangen::Man::new(cmd);
     let mut buffer: Vec<u8> = Default::default();
